0dc3b28d33b51cfdfe278a223105947246f51b62
Colby Williams <colbylwilliams@gmail.com>
2016-04-06 14:52:49 -0700

    first run

---
+++ FHashtag/FHashtag/Main.fs
@@ -0,0 +1,9 @@
+﻿namespace FHashtag
+
+open UIKit
+
+module Main = 
+    [<EntryPoint>]
+    let main args = 
+        UIApplication.Main(args, null, "AppDelegate")
+        0
---
+++ FHashtag/FHashtag/PersonTvc.fs
@@ -0,0 +1,14 @@
+﻿namespace FHashtag
+
+open System
+
+open Foundation
+open UIKit
+
+[<Register ("PersonTvc")>]
+type PersonTvc (handle:IntPtr) =
+    inherit UITableViewController (handle)
+
+    override x.NumberOfSections (tableView:UITableView) = 1
+
+
